ba03ab2bc1ad10ed8893bfb3f6ecd4f40fe8292c
Joao Bianchi <joaobianchi@live.com>
2023-09-27 17:52:54 -0300

    :construction: added termion && implemented escape key edittor exit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+use std::io::{self, stdout, Read};
+use termion::raw::IntoRawMode;
+
+fn to_control_byte(c: char) -> u8 {
+    let byte = c as u8;
+    byte & 0b001_1111
+}
+
+fn die(e: std::io::Error) {
+    panic!("{}", e);
+}
+
 fn main() {
-    println!("Hello, world!");
+    let _stdout = stdout().into_raw_mode().unwrap();
+
+    for b in io::stdin().bytes() {
+        match b {
+            Err(err) => die(err),
+            Ok(b) => {
+                let c = b as char;
+
+                if c.is_control() {
+                    println!("{:?} \r", b);
+                } else {
+                    println!("{:?} ({})\r", b, c);
+                }
+
+                if b == to_control_byte('q') {
+                    break;
+                }
+            }
+        }
+    }
 }
